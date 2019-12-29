f37eb23cfa83a548a99b1296b6d49ad5abdadee0
Amirmohammad Abedini <Amirmohammad.Abedini@gmail.com>
2019-12-30 01:00:20 +0330

    did something with var 1

--- Abjad/Code/Variant1.cs
+++ Abjad/Code/Variant1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Abjad.Code
 {
@@ -13,7 +15,7 @@ namespace Abjad.Code
 		{
 		}
 
-		public void Fill(string input)
+		public override void Fill(string input)
 		{
 			if (input.Length > MAX_INPUT_SIZE)
 			{
@@ -21,18 +23,71 @@ namespace Abjad.Code
 			}
 
 			FillMidTable(input);
+			FillTableAndCalculate(input.Length);
 		}
 
-		public void FillMidTable(string input)
+		public void CalculateScores(int colNum)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < colNum; j++)
+				{
+					Scores[i] += MapTable.MapRow1[Table[i][j]].Values;
+					NazirScores[i] += MapTable.MapRow1[Table[i][j]].NaValues;
+
+				}
+			}
+		}
+
+		private void FillTableAndCalculate(int length)
+		{
+			int partitionSize = 0;
+			bool odd = false;
+			if (length % 2 == 0)
+			{
+				partitionSize = length / 4;
+			}
+			else
+			{
+				partitionSize = length / 4 + 1;
+				odd = true;
+			}
+
+			int i = 0, j = 0;
+			int i2 = 0, j2 = 0;
+			for (i = 0; i < 3; i++)
+			{
+				for (int temp = partitionSize; temp < 0; temp--)
+				{
+					try
+					{
+						Table[i][j] = MidTable[i2][j2];
+						++j;
+						i2 = 1;
+						Table[i][j] = MidTable[i2][j2];
+						++j;
+						++j2;
+						i2 = 0;
+					}
+					catch (Exception e)
+					{
+						MessageBox.Show(e.StackTrace, "oops! something went wrong at var1 FillTableAndCalculate",
+							MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+				}
+			}
+
+			CalculateScores(partitionSize);
+		}
+
+		private void FillMidTable(string input)
 		{
 			char currentChar;
-
 			int length = input.Length;
 			if (length % 2 == 0) //even input length
 			{
 				int i = 0;
 				int j = 0;
-
 				for (int cn = 0; cn < length / 2; cn++)
 				{
 					currentChar = input[cn];
@@ -42,8 +97,9 @@ namespace Abjad.Code
 
 				string substring = input.Substring(length / 2);
 				string inputReverse = substring.Reverse().ToString();
-                //that was wrong buddy :)
-				++i;
+				//that was wrong buddy :) 
+				//stfu bitch
+				i = 1;
 				j = 0;
 				for (int cn = 0; cn < length / 2; cn++)
 				{
@@ -65,7 +121,7 @@ namespace Abjad.Code
 
 				string substring = input.Substring(length / 2 + 1);
 				string inputReverse = (string) substring.Reverse();
-				++i;
+				i = 1;
 				j = 0;
 				for (int cn = 0; cn < length / 2; cn++)
 				{
